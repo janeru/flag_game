12388c0d6f680a55e13ba62e9a056bfc0d1c693d
RU <b03801053@ntu.edu.tw>
2018-07-24 11:14:10 +0800

    flag main js as parent component

--- src/compoents/App.js
+++ src/compoents/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../compoents/css/App.css';
 import NationOption from '../compoents/NationOption';
-import Header from '../compoents/header'
+import Header from '../compoents/header';
 
 const App = () => (
   <div>
