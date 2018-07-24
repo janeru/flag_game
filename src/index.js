9e49ebbbf2a595bf0da7724f3c3ddf2fbe704cae
RU <b03801053@ntu.edu.tw>
2018-07-24 10:52:12 +0800

    react root js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './compoents/App';
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
 
